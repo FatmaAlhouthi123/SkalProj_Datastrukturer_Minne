fd207822b26fea44831e5a5338d408c3d86f7e18
Fatma Alhouthi <fa222nx@student.lnu.se>
2020-09-15 14:46:22 +0200

    Add project files.

---
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SkalProj_Datastrukturer_Minne {
+    class Program {
+
+        static List<string> theList = new List<string>();
+        static Queue iCAQueue = new Queue();
+        static bool doLoop = true;
+
+        /// <summary>
+        /// The main method, vill handle the menues for the program
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main() {
+
+            while (true)
+            {
+                Console.WriteLine("Please navigate through the menu by inputting the number \n(1, 2, 3 ,4, 0) of your choice"
+                    + "\n1. Examine a List"
+                    + "\n2. Examine a Queue"
+                    + "\n3. Examine a Stack"
+                    + "\n4. CheckParanthesis"
+                    + "\n0. Exit the application");
+                char input = ' '; //Creates the character input to be used with the switch-case below.
+                try
+                {
+                    input = Console.ReadLine()[0]; //Tries to set input to the first char in an input line
+                }
+                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter some input!");
+                }
+                if (input != '4')
+                Console.WriteLine("To add item to the list type  +itemName    to remove item from list  type -itemName");
+                switch (input)
+                {
+                    case '1':
+                        ExamineList();
+                        break;
+                    case '2':
+                        ExamineQueue();
+                        break;
+                    case '3':
+                        ExamineStack();
+                        Console.WriteLine("to get a reversed text enter word");
+                        Console.WriteLine(ReverseTextMetod());
+
+                        break;
+                    case '4':
+                        CheckParanthesis();
+                        break;
+                    /*
+                     * Extend the menu to include the recursive 
+                     * and iterative exercises.
+                     */
+                    case '0':
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Please enter some valid input (0, 1, 2, 3, 4)");
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Examines the datastructure List
+        /// </summary>
+        static void ExamineList() {
+
+            /*
+             * Loop this method untill the user inputs something to exit to main menue.
+             * Create a switch statement with cases '+' and '-'
+             * '+': Add the rest of the input to the list (The user could write +Adam and "Adam" would be added to the list)
+             * '-': Remove the rest of the input from the list (The user could write -Adam and "Adam" would be removed from the list)
+             * In both cases, look at the count and capacity of the list
+             * As a default case, tell them to use only + or -
+             * Below you can see some inspirational code to begin working.
+            */
+            //    bool doLoop = true;
+            while (doLoop)
+            {
+
+                string input = Console.ReadLine();
+                char nav = input[0];
+                string value = input.Substring(1);
+
+
+                switch (nav)
+                {
+                    case '+':
+                        theList.Add(value);
+                        Console.WriteLine("capacity" + theList.Capacity);
+                        Console.WriteLine("elements " + theList.Count);
+                        doLoop = false;
+                        break;
+
+                    case '-':
+                        theList.Remove(value);
+                        Console.WriteLine("capacity" + theList.Capacity);
+                        Console.WriteLine("elements " + theList.Count);
+                        doLoop = false;
+                        break;
+
+                    default: Console.WriteLine("Use only + or -  at the begining"); break;
+                }
+            }
+        }
+
+
+
+        /// <summary>
+        /// Examines the datastructure Queue
+        /// </summary>
+        static void ExamineQueue() {
+
+
+            /*
+             * Loop this method untill the user inputs something to exit to main menue.
+             * Create a switch with cases to enqueue items or dequeue items
+             * Make sure to look at the queue after Enqueueing and Dequeueing to see how it behaves
+            */
+
+
+            while (doLoop)
+            {
+
+                string input = Console.ReadLine();
+                char nav = input[0];
+                string value = input.Substring(1);
+
+
+                TestQueue();
+                switch (nav)
+                {
+                    case '+':
+
+
+                        iCAQueue.Enqueue(value);
+                        PeopleInQueue();
+                        doLoop = false;
+                        break;
+
+
+                    case '-':
+
+
+
+                        iCAQueue.Dequeue();
+                        PeopleInQueue();
+                        doLoop = false;
+                        break;
+
+                    default: Console.WriteLine("Use only + or -  at the begining"); break;
+
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Examines the datastructure Stack
+        /// </summary>
+        /// 
+
+
+
+
+        static void ExamineStack() {
+            Stack myStack = new Stack();
+            /*
+             * Loop this method until the user inputs something to exit to main menue.
+             * Create a switch with cases to push or pop items
+             * Make sure to look at the stack after pushing and and poping to see how it behaves
+            */
+
+
+
+            while (doLoop)
+            {
+
+                string input = Console.ReadLine();
+                char nav = input[0];
+                string value = input.Substring(1);
+
+
+                switch (nav)
+                {
+                    case '+':
+                        myStack.Push(value);
+
+
+                        doLoop = false;
+                        break;
+
+                    case '-':
+
+                        myStack.Pop();
+
+                        doLoop = false;
+                        break;
+
+
+                    default: Console.WriteLine("Use only + or -  at the begining"); break;
+                }
+
+
+
+            }
+
+        }
+
+
+        static void PeopleInQueue() {
+
+            Console.WriteLine("people in queue");
+            foreach (var cutomer in iCAQueue)
+            {
+                Console.WriteLine(cutomer);
+            }
+        }
+
+        static void TestQueue() {
+
+            iCAQueue.Enqueue("Kalle");
+            iCAQueue.Enqueue("Greta");
+            iCAQueue.Dequeue();
+            iCAQueue.Enqueue("Stina");
+            iCAQueue.Dequeue();
+            iCAQueue.Enqueue("Ole");
+
+
+
+        }
+
+
+        static string ReverseTextMetod() {
+            Stack myStack = new Stack();
+            string reversedInput = "";
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                myStack.Push(input[i]);
+            }
+
+            foreach (var item in myStack)
+            {
+                reversedInput += item;
+            }
+
+            return reversedInput;
+
+
+
+        }
+        static void CheckParanthesis() {
+            /*
+             * Use this method to check if the paranthesis in a string is Correct or incorrect.
+             * Example of correct: (()), {}, [({})],  List<int> list = new List<int>() { 1, 2, 3, 4 };
+             * Example of incorrect: (()]), [), {[()}],  List<int> list = new List<int>() { 1, 2, 3, 4 );
+             */
+            List<char> list_Of_Open_Paranthesis = new List<char>() { '<', '{', '[', '(' };
+            List<char> list_Of_Close_Paranthesis = new List<char>() { '>', '}', ']', ')' };
+            List<char> myList = new List<char>();
+            string input = "(()), {}, [({})],";
+            //int listSize = myList.Count;
+            char hold =' ';
+
+           
+            
+          
+
+
+
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < list_Of_Open_Paranthesis.Count; j++)
+                {
+                      if (input[i] == list_Of_Open_Paranthesis[j])
+                    {
+                            hold = input[i];
+                      
+                        myList.Add(input[i]);
+                    }
+                        else if (input[i] == list_Of_Close_Paranthesis[j] & hold == list_Of_Open_Paranthesis[j])
+                        { 
+                      
+                        myList.Remove(hold);
+                      
+
+                    }
+                
+        
+                    }
+                            }
+
+            if (myList.Count == 0)
+                Console.WriteLine("correct order");
+            else
+                Console.WriteLine("not  correct order");
+            foreach (var item in myList) Console.WriteLine(item);
+            {
+
+            }
+        }  
+    
+    
+   
+    
+    }
+}
+
+
---
+++ SkalProj_Datastrukturer_Minne/QuestionsAndAnswer.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkalProj_Datastrukturer_Minne {
+
+    class QuestionsAndAnswer {
+
+
+        //    //    //                       // ///             Övning 1: ExamineList()
+        
+        // 1. Hur fungerar stacken och heapen ? Förklara gärna med exempel eller skiss på dess grundläggande funktion.
+
+        /* 
+        - Stacken används för att lagra lokala variabler av value typer till exempel int, double , long och refrence variabler till exemple 
+        employee, student, car refrence variables.
+        och metoder anrop DoThing() exempelvis.
+        Variabler i stacken köras i tur ordning, stacken tar bort dem när de är färdigkörs.
+        -Heapen används för att lagra klass, objekt, listor, ..osv  samt instatns variabler som är en del av klassen och är inte lokala variabler.
+        Heapen tar inte bort själv objekter efter den körsfärdiga utan det är GC som tar hand om det.
+         */
+
+        //2. Vad är Value Types repsektive Reference Types och vad skiljer dem åt?
+
+        /*
+         Value Typer är variabler som lagras i stacken -eller heapen om den är instatns variabler och är en del av klassen- till exempel 
+        Int, double,bool values 
+         referens Typer är objekter som lagras i Heapen och har en referns namn som lagras i stacken.
+
+         */
+
+        //3. Följande metoder ( se bild nedan ) genererar olika svar. Den första returnerar 3, den
+        // andra returnerar 4, varför?
+
+
+        /*
+          Iförsta bilden vi har 2 variabler x, y type Integer som  har 2 olika värde  från början x = 0; och y = 0; sen x, y tildellas nya 
+        värde 
+         x=3; y=x; alstå y fick samma värde som x  y = 3;
+        sen y värde ändras till 4 y=4;  men x behållade den gamla värde x= 3; det är därför  första koden i bilden1 retunerar 3
+
+         I bild2
+
+        Vi skapade  objekt x som har värde MyValue =3  
+        sen skapade vi objekt y 
+        sen y = x och det betyder att referns y pekar på objekt x nu och att y objekt blev utan referens så GC kan tar bort y objekt 
+        nu
+        nu vi har vi en x object som har 2 referenser x och y
+        och vi kan ändra property för x obj antengen me x referens eller y referens 
+        y.MyValue = 4   är exakt samma om vi skriver x.MyValue = 4
+        det är därför vi fick 4 i andra bilden
+         */
+
+        //2. När ökar listans kapacitet? (Alltså den underliggande arrayens storlek)?
+        //3.Med hur mycket ökar kapaciteten?
+
+        /*
+         Först list kapacitet är 0 så fort vi ligger ett objekt list kapacitet ökar med 4 om vi ligger mer än 4 objecter eller variabler i listan ökar kapacitet 
+        till 8 
+        om vi förtsätta ligga mer än 8 objekter ökar kapacitet till 16  
+        ligga mer än 16 objekter ökar kapacitet till 32   och sen till 64, 128, 256 ,.. osv.
+         */
+        //4.Varför ökar inte listans kapacitet i samma takt som element läggs till?
+
+        /*
+         List ökar inte när man ligga ett objekt till list utan den har fixed kapacitet från början , om man ligga mer objekter och finns inte mer tillgängliga platser
+        för nya objekter dotnet ramverk skapar ett nytt list med double kapacitet och kopera den gamal list till den nya listan automatisk och på det här sätet ökar 
+        kapacitet till den nya list när den blir fullt med objekter.         
+         */
+        //5. Minskar kapaciteten när element tas bort ur listan?
+
+        /*
+         Nej kapaciteten ska inte minskar
+         */
+
+        //6. När är det då fördelaktigt att använda en egendefinierad array istället för en lista?
+
+        /*
+          När man har bestämd och fixed antal av objekter som ska lagras i arrayen        
+         
+         */
+
+
+
+        //                                                 Övning 3: ExamineStack()
+
+
+
+
+        //        1. Simulera ännu en gång ICA-kön på papper.Denna gång med en stack.Varför är det
+        //inte så smart att använda en stack i det här fallet?
+
+        /*
+         * för att den sist som kommer in i kön expedieras först 
+         */
+
+        //        2. Implementera en ReverseText-metod som läser in en sträng från användaren och
+        //med hjälp av en stack vänder ordning på teckenföljden för att sedan skriva ut den
+        //omvända strängen till användaren.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
